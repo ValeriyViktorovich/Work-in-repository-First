6e9aa929c1a4295ca5046c7512e83e7b468a0846
Valeriy Zhilin <zhilinvv@rambler.ru>
2023-12-26 13:49:04 +0500

    Process studien GIT

---
+++ Automobile.py
@@ -0,0 +1,37 @@
+class Automobil():
+    def __init__(self, type1, marka, speed, massa, long, oil, rashodOil, probeg, damage):
+        self.type1 = type1
+        self.marka = marka
+        self.speed = speed
+        self.massa = massa
+        self.long = long
+        self.oil = oil
+        self.rashodOil = rashodOil
+        self.probeg = probeg
+        self.damage = damage
+
+    def stand():
+        return
+        if speed == 0
+
+
+class Drivers():
+    def __init__(self, old, health, risk, stazh): class drivers
+        self.old = old
+        self.health = health
+        self.risk = risk
+        self.stazh = stazh
+
+
+class Road():
+    def__init__(self, surface, main_road, circkle, remont, proezhaj_chast, obochina):
+    self.surface = surface
+    self.main_road = main_road
+    self.circkle = circkle
+    self.remont = remont
+    self.proezhaj_chast = proezhaj_chast
+    self.obochina = obochina
+
+
+class Distance():
+    def__init__(self, begin1, time1, goal, leng, current_position):
