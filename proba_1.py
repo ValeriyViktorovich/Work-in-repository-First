951d4f09dc6981adbd37230e7e550f462c6935e3
Valeriy Zhilin <zhilinvv@rambler.ru>
2024-01-11 17:58:56 +0500

    Another version file Proba_1

---
+++ proba_1.py
@@ -0,0 +1,8 @@
+a = 1
+print("Валера учусь пробую  работаю")
+exponential_num = 1.5e-03
+decimal_num = float(exponential_num)  # Ничего не меняю просто добавил комментарий
+a = a + 2 * a
+print(a)
+print(exponential_num)
+print(decimal_num)
