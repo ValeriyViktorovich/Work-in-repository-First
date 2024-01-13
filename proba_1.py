ca4da67d3f4d1f0554999e665050f01f511cf522
Valeriy Zhilin <zhilinvv@rambler.ru>
2024-01-13 09:46:14 +0500

    Choose fail on right file

--- proba_1.py
+++ proba_1.py
@@ -1,9 +1,17 @@
 a = 1
 print("Валера учусь пробую  работаю")
 exponential_num = 1.5e-03
+<<<<<<< HEAD
+decimal_num = float(exponential_num)  # Ничего не меняю просто добавил комментарий
+a = a + 2 * a
+print(a)
+print(exponential_num)
+print(decimal_num)
+=======
 decimal_num = float(exponential_num)# Ничего не меняю просто добавил комментарий
 a=a+2*a
 print a
 print exponential_num
 print decimal_num
 
+>>>>>>> origin/master
